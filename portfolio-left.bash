9f1652633cab5184db5f5200c5de44cfdc822f58
Barry Jackson <43217434+btjackson-usgs@users.noreply.github.com>
2022-07-23 12:39:02 -0500

    Add files via upload

---
+++ portfolio-left.bash
@@ -0,0 +1,46 @@
+  <section id=fatbanker class="about_section about_section1 layout_padding-bottom">
+    <div class="container  ">
+      <div class="row">
+        <div class="col-lg-6 ">
+          <div class="img-box">
+            <img class="fatbanker" alt="Fat Banker Logo"
+			src="images/fatbanker-reg.jpg" >
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="detail-box">
+            <div class="heading_container" border=1>
+              <h2>
+                GREEDY BANKER
+              </h2>
+            </div>
+            <p>
+This program runs from the command line. This same method could be used to quickly and easily create a crypto bot. This program finds real-time transactions that are over a million bucks. Meaning notify us when someone makes a large purchase or sale. You may want to do the same. Version 1 your hear a register, Version 2 emails you when a large transaction occurs. 
+            </p>
+	<div class="row">
+<div class="pull-left">
+    <button type="button" onclick=window.location='https://github.com/btjackson-usgs/fatbanker'
+	class="btn btn-default btn-lg green_button">
+	SOURCE CODE
+        <span class="glyphicon glyphicon-chevron-left"></span>
+     </button>
+ </div>
+
+
+ <div class="pull-right">
+    <button type="button"
+	class="red_button btn btn-default btn-lg">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+	  VERSION 1
+    </button>
+ </div>
+			
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end about section -->
+
